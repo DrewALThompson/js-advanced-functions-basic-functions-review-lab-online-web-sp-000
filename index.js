c02b5b5807507fe928b8496ebd131e424e2eec34
Drew Thompson <drewallynthompson@gmail.com>
2021-01-13 08:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function mondayWork(activity = 'go to the office'){
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(){}
+function wrapAdjective(){
+  
+}
 
 let Caluctlator = {}
