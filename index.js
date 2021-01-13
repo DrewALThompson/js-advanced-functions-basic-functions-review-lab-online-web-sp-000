263f6b755793ba9a9a7b6605fe020635ef09ffbb
Drew Thompson <drewallynthompson@gmail.com>
2021-01-13 09:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ function wrapAdjective(string = '*'){
 }
 
 let Calculator = {
-  add: function(){
-    
+  add: function(a, b){
+    return a+b;
+  },
+  subtract: function(a, b){
+    return a-b; 
+  },
+  multiply: function(a, b){
+   return a*b; 
+  },
+  divide: function(a, b){
+   return a/b; 
   }
 }
